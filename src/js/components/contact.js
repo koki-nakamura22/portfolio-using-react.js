8a45ee802d504f7141041cc484959875b3fa0763
Koki Nakamura <nk.levia22@gmail.com>
2019-06-29 21:36:38 +1000

    Added "contact" id tag to Contact section.
    Added "contact" id tag to Contact section.

--- src/js/components/contact.js
+++ src/js/components/contact.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import Utils from '../utils/utils';
-import profileData from './../../json/profile.json';
-import contactData from './../../json/contact.json';
+import React, { Component } from "react";
+import Utils from "../utils/utils";
+import profileData from "./../../json/profile.json";
+import contactData from "./../../json/contact.json";
 
 class Contact extends Component {
   constructor(props) {
@@ -11,7 +11,7 @@ class Contact extends Component {
       name: "",
       subject: "",
       message: ""
-    }
+    };
 
     this.onChangeName = this.onChangeName.bind(this);
     this.onChangeSubject = this.onChangeSubject.bind(this);
@@ -20,15 +20,15 @@ class Contact extends Component {
   }
 
   onChangeName(e) {
-    this.setState({name: e.target.value});
+    this.setState({ name: e.target.value });
   }
 
   onChangeSubject(e) {
-    this.setState({subject: e.target.value});
+    this.setState({ subject: e.target.value });
   }
 
   onChangeMessage(e) {
-    this.setState({message: e.target.value});
+    this.setState({ message: e.target.value });
   }
 
   submit(e) {
@@ -40,80 +40,105 @@ class Contact extends Component {
     for (let line of lines) {
       message += `${line}${newLineCharsMail}`;
     }
-const overallMessage = `Hi! I'm ${this.state.name}. I will send you an e-mail from your portfolio page.
+    const overallMessage = `Hi! I'm ${
+      this.state.name
+    }. I will send you an e-mail from your portfolio page.
     ${newLineCharsMail}${newLineCharsMail}${message}${newLineCharsMail}`;
-    window.location.href = `mailto:${profileData.email}?subject=${this.state.subject}&body=${overallMessage}`;
+    window.location.href = `mailto:${profileData.email}?subject=${
+      this.state.subject
+    }&body=${overallMessage}`;
   }
 
   render() {
     return (
-      <section className="section">
-        <form onSubmit={this.submit}>
-          <div className="container">
-            <div className="section-heading">
-              <h3 className="title is-2">{Utils.escapeHtml(contactData.title)}</h3>
-              <h4 className="subtitle is-5">{Utils.escapeHtml(contactData.subTitle)}</h4>
-            </div>
-            <br />
+      <div id="contact">
+        <section className="section">
+          <form onSubmit={this.submit}>
+            <div className="container">
+              <div className="section-heading">
+                <h3 className="title is-2">
+                  {Utils.escapeHtml(contactData.title)}
+                </h3>
+                <h4 className="subtitle is-5">
+                  {Utils.escapeHtml(contactData.subTitle)}
+                </h4>
+              </div>
+              <br />
 
-            <div className="columns">
-              <div className="column is-6 is-offset-3">
-                <div className="box">
-                  <div className="field">
-                    <label className="label">{Utils.escapeHtml(contactData.nameLabel)}</label>
-                    <div className="control">
-                      <input
-                        id="name"
-                        className="input"
-                        type="text"
-                        placeholder="Name input"
-                        value={this.state.name}
-                        onChange={this.onChangeName}
-                      />
+              <div className="columns">
+                <div className="column is-6 is-offset-3">
+                  <div className="box">
+                    <div className="field">
+                      <label className="label">
+                        {Utils.escapeHtml(contactData.nameLabel)}
+                      </label>
+                      <div className="control">
+                        <input
+                          id="name"
+                          className="input"
+                          type="text"
+                          placeholder="Name input"
+                          value={this.state.name}
+                          onChange={this.onChangeName}
+                        />
+                      </div>
                     </div>
-                  </div>
 
-                  <div className="field">
-                    <label className="label">{Utils.escapeHtml(contactData.subjectLabel)}</label>
-                    <div className="control">
-                      <input
-                        id="subject"
-                        className="input"
-                        type="text"
-                        placeholder="Subject input"
-                        value={this.state.value}
-                        onChange={this.onChangeSubject}
-                      />
+                    <div className="field">
+                      <label className="label">
+                        {Utils.escapeHtml(contactData.subjectLabel)}
+                      </label>
+                      <div className="control">
+                        <input
+                          id="subject"
+                          className="input"
+                          type="text"
+                          placeholder="Subject input"
+                          value={this.state.value}
+                          onChange={this.onChangeSubject}
+                        />
+                      </div>
                     </div>
-                  </div>
 
-                  <div className="field">
-                    <label className="label">{Utils.escapeHtml(contactData.messageLabel)}</label>
-                    <div className="control">
-                      <textarea
-                        id="message"
-                        className="textarea"
-                        placeholder="Message input"
-                        value={this.state.value}
-                        onChange={this.onChangeMessage}
-                      ></textarea>
+                    <div className="field">
+                      <label className="label">
+                        {Utils.escapeHtml(contactData.messageLabel)}
+                      </label>
+                      <div className="control">
+                        <textarea
+                          id="message"
+                          className="textarea"
+                          placeholder="Message input"
+                          value={this.state.value}
+                          onChange={this.onChangeMessage}
+                        />
+                      </div>
                     </div>
-                  </div>
 
-                  <div className="field is-grouped has-text-centered">
-                    <div className="control">
-                      <button type="submit" id="submit-button" className="button is-link is-large">
-                        <span className="icon"> <i className="fas fa-envelope"></i> </span>
-                        <span>{Utils.escapeHtml(contactData.submitLabel)}</span>
-                      </button>
+                    <div className="field is-grouped has-text-centered">
+                      <div className="control">
+                        <button
+                          type="submit"
+                          id="submit-button"
+                          className="button is-link is-large"
+                        >
+                          <span className="icon">
+                            {" "}
+                            <i className="fas fa-envelope" />{" "}
+                          </span>
+                          <span>
+                            {Utils.escapeHtml(contactData.submitLabel)}
+                          </span>
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </form>
-      </section>
+          </form>
+        </section>
+      </div>
     );
   }
 }
