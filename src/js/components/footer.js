1f572e4f600e0630e872abf70e77e98e73e7240c
Koki Nakamura <nk.levia22@gmail.com>
2020-04-29 22:46:26 +1000

    Fixed footer layout, and added current year to copyright.

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -9,16 +9,16 @@ class Footer extends Component {
             <strong>Koki Nakamura's portfolio page</strong> by Koki Nakamura.
           </p>
           <p>
-            &copy; 2019 Koki Nakamura
+            &copy; 2019-2020 Koki Nakamura
           </p>
           <p>
-            The source code is licensed
+            The source code is licensed&nbsp;
             <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
           </p>
           <p>
-            This web page is created based on 
+            This web page is created based on&nbsp;
             <a href="https://github.com/dansup/bulma-templates/blob/master/templates/personal.html">Portfolio</a>
-            in 
+            &nbsp;in&nbsp;
             <a href="https://github.com/dansup/bulma-templates">Bulma Templates.</a>
           </p>
         </div>
